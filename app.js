057cc50d57ce70c55d2e8777bcea139e69f8cfe9
M Huzaifa Nizami <153551455+MHuzaifanizami@users.noreply.github.com>
2024-05-08 11:47:27 +0500

    file updated

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ function count() {
     var eidDay = new Date("16 june 2024")
     var date = new Date();
     var diff = eidDay - date
-    inputs[0].value = Math.floor(diff / (1000 * 60 * 60 * 24));
-    inputs[1].value = Math.floor(diff / (1000 * 60 * 60));
-    inputs[2].value = Math.floor(diff / (1000 * 60));
-    inputs[3].value = Math.floor(diff / 1000);
+    if (diff <= 0) {
+
+        return alert('Eid Mubarak')
+
+    } else {
+        inputs[0].value = Math.floor(diff / (1000 * 60 * 60 * 24));
+        inputs[1].value = Math.floor(diff / (1000 * 60 * 60));
+        inputs[2].value = Math.floor(diff / (1000 * 60));
+        inputs[3].value = Math.floor(diff / 1000);
+
+    }
 }
 
-count()
 
+count()
 
 setInterval(
 
